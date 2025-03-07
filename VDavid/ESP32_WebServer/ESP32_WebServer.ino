6a53e72f00a6eb734357c9713e6338b78352e9fb
MiguelRodrg <112191313+MiguelRodrg@users.noreply.github.com>
2025-03-07 09:07:57 -0500

    V4.1
    Validaciones adicionales agregadas

--- VDavid/ESP32_WebServer/ESP32_WebServer.ino
+++ VDavid/ESP32_WebServer/ESP32_WebServer.ino
@@ -10,6 +10,7 @@
 #include <ESP32Servo.h>
 
 // Objetos servo
+int conteo = 20;
 Servo servo1;
 
 // Variables a usar
@@ -18,17 +19,24 @@ int ang = 0;
 // Parametros de configuracion
 const int pin_led = 5;
 const int pin_servo = 18;
-const char* ap_ssid = "AP_ESP32_WEB_SERVER";
-const char* ap_password = "PROTOBOARDISMINE";
+const char* ap_ssid = "Perry";
+const char* ap_password = "t4y5u6i7o8";
 
 // Crea el servidor web en el puerto indicado
 WebServer server(80);
 
-// [FUNCIONES HANDLE DE  CONTROL REMOTO]
+
+// Función para manejar CORS
+void handleCors() {
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  server.sendHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  server.sendHeader("Access-Control-Allow-Headers", "Content-Type");
+}
 
 // Funcion LED ON
 void handle_led_on() {
   digitalWrite(pin_led, HIGH);
+  handleCors();  // Agregar encabezados CORS
   server.send(200, "text/plain", "LED ENCENDIDO");
   Serial.println("LED ENCENDIDO");
 }
@@ -36,6 +44,7 @@ void handle_led_on() {
 // Funcion LED OFF
 void handle_led_off() {
   digitalWrite(pin_led, LOW);
+  handleCors();
   server.send(200, "text/plain", "LED APAGADO");
   Serial.println("LED APAGADO");
 }
@@ -45,6 +54,8 @@ void handle_servo_up() {
   ang = servo1.read() + 5;
   Serial.print("SERVO U = "); Serial.println(ang);
   servo1.write(ang);
+  handleCors();
+  server.send(200, "text/plain", "SERVO ARRIBA");
 }
 
 // Funcion SERVO ABAJO
@@ -52,39 +63,51 @@ void handle_servo_down() {
   ang = servo1.read() - 5;
   Serial.print("SERVO D = "); Serial.println(ang);
   servo1.write(ang);
+  handleCors();
+  server.send(200, "text/plain", "SERVO ABAJO");
+}
+
+// Manejo de solicitudes OPTIONS para CORS
+void handleOptions() {
+  handleCors();
+  server.send(204); // Respuesta sin contenido
 }
 
 // [SETUP DE ESP32]
 void setup() {
-
-  // Pines y comunicacion
+  // Pines y comunicación
   Serial.begin(115200);
   delay(1000);
 
   servo1.attach(pin_servo);
-
   pinMode(pin_led, OUTPUT);
 
   // Configurar el Punto de Accseso a ESP32
   Serial.println("Configurando el punto de acceso como: " + String(ap_ssid));
   WiFi.mode(WIFI_AP);
   WiFi.softAP(ap_ssid, ap_password);
-  Serial.println("Configuracion exitosa en la direccion IP: " + String(WiFi.softAPIP()));
+  Serial.print("Configuracion exitosa en la direccion IP: ");
   Serial.println(WiFi.softAPIP());
 
   // Declaracion de las rutas para funciones handle
-  server.on("/led_on", handle_led_on);
-  server.on("/led_off", handle_led_off);
-  server.on("/servo_up", handle_servo_up);
-  server.on("/servo_down", handle_servo_down);
+  server.on("/led_on", HTTP_GET, handle_led_on);
+  server.on("/led_off", HTTP_GET, handle_led_off);
+  server.on("/servo_up", HTTP_GET, handle_servo_up);
+  server.on("/servo_down", HTTP_GET, handle_servo_down);
+
+  // Manejo de preflight requests para CORS
+  server.onNotFound(handleOptions);
 
   // Iniciar el servidor
   server.begin();
-  Serial.println("Servidor HTTP iniciado con exito!");
+  Serial.println("Servidor HTTP iniciado con éxito!");
 }
 
 // [LOOP DE ESP32]
 void loop() {
+  if (conteo > 0) {
+    Serial.println(WiFi.softAPIP());
+    conteo--;
+  }
   server.handleClient();
-  delay(1000);
 }
